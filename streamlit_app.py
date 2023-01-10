ff6b5311f1eeedf63d898b17f0c08922dfb4f396
vashwani <114215765+vashwani@users.noreply.github.com>
2023-01-10 10:45:22 +0530

    Update streamlit_app.py
    adding pandas and a list using dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ streamlit.text('Cloud Data Build')
 streamlit.text('Data Engineering')
 streamlit.text('Bussiness Analytics')
 streamlit.text('Others')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
